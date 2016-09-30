acd057ccfe30ced2f3dbe556b27712ef21242b14
Gaurav Srivastava <gaurav.srivastava7@gmail.com>
2016-09-30 02:34:50 -0400

    Automatically reading the packages available so that we can choose which apps to start randomly

--- monkeyrun.py
+++ monkeyrun.py
@@ -8,6 +8,21 @@ print "start"
 # connection to the current device, and return a MonkeyDevice object
 device = MonkeyRunner.waitForConnection()
 
+
+# List all packages installed
+
+installed_packages = device.shell('pm list packages -f -3')
+for app in installed_packages.split():
+    package_name = app.split("=")[1]
+    apk_path = device.shell('pm path ' + package_name)
+    if apk_path.startswith('package:'):
+        print package_name + " already installed."
+        print "Apk Path is " + apk_path.split(":")[1]
+    else:
+        print package_name + " not installed"
+    MonkeyRunner.sleep(2)
+    
+'''
 apk_path = device.shell('pm path com.erigo.cowise')
 if apk_path.startswith('package:'):
     print "Cowise already installed."
@@ -23,5 +38,5 @@ MonkeyRunner.sleep(10)
 
 #sending an event which simulate a click on the menu button
 device.touch(591, 1660, MonkeyDevice.DOWN_AND_UP)
-
+'''
 print "end of script"
