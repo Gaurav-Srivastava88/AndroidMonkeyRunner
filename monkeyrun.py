c5f54cd2baae88a84343dda0dd2f78090838a6b8
Gaurav Srivastava <gaurav.srivastava7@gmail.com>
2016-09-30 21:22:05 -0400

    1. Get Launcher Acitivity 2. Randomly choose 5 apps to start and interact with 3. Function to close an app

--- monkeyrun.py
+++ monkeyrun.py
@@ -1,6 +1,48 @@
 from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice
 import commands
 import sys
+import random
+
+
+def get_launcher_activity(package_name):
+    tmp = device.shell('pm dump ' + package_name + ' | grep -A 1 MAIN | grep -i -w ' + package_name) 
+    #print tmp
+
+    print tmp
+    
+    for c in tmp.split(" "):
+        if package_name in c:
+            print c
+            launcher_activity = c.split()[0]
+            print launcher_activity
+            break
+    return launcher_activity
+	
+	
+def get_rand_app(installed_packages):
+    all_apps = installed_packages.split()
+    max = len(all_apps)
+	
+    sample_no = random.sample(range(1, max), 5)
+    sample_apps = {}
+    for no in sample_no:
+        sample_apps[all_apps[no]] = ""
+    return sample_apps    
+    
+def stop_app(package_name):
+    # Simulates Closing an application by user
+    MonkeyRunner.sleep(5)
+        
+    device.shell('am force-stop ' + package_name)
+
+
+def remove_one_recent_app():
+    MonkeyRunner.sleep(2)
+    device.press('KEYCODE_APP_SWITCH', MonkeyDevice.DOWN_AND_UP)
+
+    MonkeyRunner.sleep(1)
+    device.drag((350, 620), (13, 620), 0.5, 50)
+
 
 # starting script
 print "start"
@@ -8,12 +50,33 @@ print "start"
 # connection to the current device, and return a MonkeyDevice object
 device = MonkeyRunner.waitForConnection()
 
+installed_packages = device.shell('pm list packages -f -3')
+
+sampled_apps = get_rand_app(installed_packages)
+
+testing_apps = {}
+
+for app in sampled_apps:
+    package_name = app.split("=")[1]
+    print package_name
+    testing_apps[package_name] = get_launcher_activity(package_name)
+
+print testing_apps
+
+'''
+for app in testing_apps:
+    device.startActivity(component=testing_apps[app])
+    MonkeyRunner.sleep(5)
+
+#stop_app('com.erigo.cowise')
+
+MonkeyRunner.sleep(5)
+remove_one_recent_app()
+'''
 
 # List all packages installed
-
-installed_packages = device.shell('pm list packages -f -3')
+'''
 for app in installed_packages.split():
-    package_name = app.split("=")[1]
     apk_path = device.shell('pm path ' + package_name)
     if apk_path.startswith('package:'):
         print package_name + " already installed."
@@ -21,7 +84,7 @@ for app in installed_packages.split():
     else:
         print package_name + " not installed"
     MonkeyRunner.sleep(2)
-    
+''' 
 '''
 apk_path = device.shell('pm path com.erigo.cowise')
 if apk_path.startswith('package:'):
